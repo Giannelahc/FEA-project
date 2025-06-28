01aa4e57a7cc719ef87a5a807ef935ad60a99b2a
Giannela Huamani <giannelahc@gmail.com>
2025-06-28 12:24:38 +0200

    restructure to twitter

---
+++ src/pages/LoginPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Homepage() {
+    return (
+        <div>
+            <h1>Welcome to Spotifake</h1>
+            <p>Discover new music, albums and playlists</p>
+            {/* You can map recommendations here */}
+        </div>
+    );
+}
+
+export default Homepage;
